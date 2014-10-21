4a6e38718e606536a0c85023d6216090cbb930f0
pyro2927 <joseph@pintozzi.com>
2014-10-20 21:41:40 -0500

    Supporting monit w/ username and password

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,11 @@
 class Service < ActiveRecord::Base
 
   def url
-    "http://#{ip_address}:#{port}/"
+    if username.nil?
+      "http://#{ip_address}:#{port}/"
+    else
+      "http://#{username}:#{password}@#{ip_address}:#{port}/"
+    end
   end
 
 end
